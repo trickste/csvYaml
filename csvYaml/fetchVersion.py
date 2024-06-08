2a164a721b30182277cb8a6ba70942590ca773e9
trickste <nishantprakashsaini285@gmail.com>
2024-06-09 00:56:14 +0530

    add: add code to fetch version

--- csvYaml/fetchVersion.py
+++ csvYaml/fetchVersion.py
@@ -1,7 +1,6 @@
 import os
 def getversion():
     '''Function to fetch version'''
-    os.system("ls")
     with open('csvYaml/version.txt', 'r', encoding='UTF-8') as file:
         version = file.readlines()
     return version[-1]
