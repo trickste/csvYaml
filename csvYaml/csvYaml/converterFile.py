2eee4513d5f0774c0df8c54734bed79f30c054f0
trickste <nishantprakashsaini285@gmail.com>
2024-06-09 22:55:06 +0530

    update: code to process csv file better

--- csvYaml/csvYaml/converterFile.py
+++ csvYaml/csvYaml/converterFile.py
@@ -76,12 +76,47 @@ def yamlToCsv(input_file, output_path):
     return output_file
 
 
+def manageDictCSV(elementList, data, output, row):
+    '''This function populates Dictionary content and checks dataype of upcoming data'''
+    print("dict: "+str(elementList))
+    if len(elementList) == 1:
+        output[elementList[0]] = data[row]
+    else:
+        if elementList[1].isdigit():
+            output[elementList[0]] = manageListCSV(elementList[1:], data, [], row)
+        else:
+            output[elementList[0]] = manageDictCSV(elementList[1:], data, output, row)
+    return output
+
+
+def manageListCSV(elementList, data, output, row):
+    '''This function populates List content and checks dataype of upcoming data'''
+    print("list: "+str(elementList))
+    if len(elementList) == 1:
+        output.append(data[row])
+        # return data[row]
+    else:
+        if elementList[1].isdigit():
+            output.append(manageListCSV(elementList[1:], data, output, row))
+        else:
+            output.append(manageDictCSV(elementList[1:], data, {}, row))
+    return output
+
+
 def conversionToYaml(data, rows, output):
     '''Function initiates conversion of content fetched from CSV file for YAML file'''
     for row in range(rows):
         val = {}
-        for element in data:
-            val[element] = data[element][row]
+        localList = []
+        for elements in data:
+            elementList = elements.split("/")
+            if len(elementList) == 1:
+                val[elements] = data[elements][row]
+            else:
+                if elementList[1].isdigit():
+                    val[elementList[0]] = manageListCSV(elementList[1:], data[elements], localList, row)
+                else:
+                    val[elementList[0]] = {}
         output.append(val)
     return output
 
